201f8ce7e7b9a8e357333be3310a670fcebe64ba
KaranAulakh <karansaulakh@gmail.com>
2025-04-18 22:50:18 -0600

    Base Interpreter rough draft

--- src/com/veld/Interpreter.java
+++ src/com/veld/Interpreter.java
@@ -1,4 +1,44 @@
 package com.veld;
 
-public class Interpreter {
+public class Interpreter
+    implements Expr.Visitor<Object> {
+
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+
+    //////////////////////////
+    ///   HELPER METHODS   ///
+    //////////////////////////
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
 }
