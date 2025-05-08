710b8605182e53834da47ad92a49eeccb7701c20
KaranAulakh <karansaulakh@gmail.com>
2025-05-07 21:31:26 -0600

    Parse Statements into the AST

--- src/com/veld/Parser.java
+++ src/com/veld/Parser.java
@@ -1,5 +1,6 @@
 package com.veld;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.veld.TokenType.*;
@@ -16,16 +17,36 @@ class Parser {
     }
 
     Expr parse() {
-        try {
-            return expression();
-        } catch (ParseError error) {
-            return null;
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(statement());
         }
+
+        return statements;
     }
 
-    //////////////////////
-    ///    OPERATORS   ///
-    //////////////////////
+    ////////////////////////////////
+    ///    HANDLING STATEMENTS   ///
+    ////////////////////////////////
+
+    private Stmt statement() {
+        if (match(PRINT)) return printStatement();
+
+        return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr expr = expression();
+        consume(SEMICOLON, "Expect ';' after expression.");
+        return new Stmt.Expression(expr);
+    }
+
     private Expr expression() {
         return equality();
     }
